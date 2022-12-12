58b94a9d01086024865d7ab164208c18144a766d
MartinelliFabio <fabio.martinelli996@gmail.com>
2022-12-12 15:41:16 +0100

    Screen printed the values ​​of the two properties

--- Models/Movie.php
+++ Models/Movie.php
@@ -1,14 +1,27 @@
 <?php
   class Movie {
-    public $nome;
-    public $autore;
+    public $titolo;
+    public $regista;
     public $genere;
+    public $minuti;
+    public $durata;
     
 
-    function __construct($_nome, $_autore, $_genere) {
-        $this->nome = $_nome;
-        $this->autore = $_autore;
+    function __construct(String $_titolo, String $_regista, String $_genere, Int $_minuti) {
+        $this->titolo = $_titolo;
+        $this->regista = $_regista;
         $this->genere = $_genere;
+        $this->minuti = $_minuti;
+
+        $this->setLenghtMovie();
+    }
+
+    public function setLenghtMovie() {
+        if($this->minuti > 90) {
+            $this->durata = "Lungo";
+        } else {
+            $this->durata = "Corto";
+        }
     }
   }
 ?>
--- index.php
+++ index.php
@@ -2,8 +2,14 @@
 
 include_once __DIR__ . './Models/Movie.php';
 
-$film = new Movie("Pulp Fiction", "Quentin Tarantino", "Azione");
+$pulp = new Movie("Pulp Fiction", "Quentin Tarantino", "Azione", 70);
+var_dump($pulp);
 
-var_dump($film);
+$kill = new Movie("Kill Bill Vol.1", "Quentin Tarantino", "Azione", 120);
+var_dump($kill);
+
+echo "Nome film: " . "<strong>$pulp->titolo</strong>" . "<br>" . " Diretto da: " . "<strong>$pulp->regista</strong>" . "<br>" . "Genere: " . "<strong>$pulp->genere</strong>" . "<br>" . "Minuti del film: " . "<strong>$pulp->minuti</strong>" . "<br>" . "Durata: " . "<strong>$pulp->durata</strong>" . "<br>" . "<br>";
+echo "Nome film: " . "<strong>$kill->titolo</strong>" . "<br>" . " Diretto da: " . "<strong>$kill->regista</strong>" . "<br>" . "Genere: " . "<strong>$kill->genere</strong>" . "<br>" . "Minuti del film: " . "<strong>$kill->minuti</strong>" . "<br>" . "Durata: " . "<strong>$kill->durata</strong>";
+
 
 ?>
